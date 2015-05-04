ebec7cd24002eee6fc0306e7e055a5839e45622c
invkrh <invkrh@gmail.com>
2015-05-04 14:30:39 +0200

    [calculator] finished

---
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,47 @@
+package calculator
+
+sealed abstract class Expr
+final case class Literal(v: Double) extends Expr
+final case class Ref(name: String) extends Expr
+final case class Plus(a: Expr, b: Expr) extends Expr
+final case class Minus(a: Expr, b: Expr) extends Expr
+final case class Times(a: Expr, b: Expr) extends Expr
+final case class Divide(a: Expr, b: Expr) extends Expr
+
+object Calculator {
+
+  var visited = Set[String]()
+
+  def computeValues(
+      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions map {
+      case (ref, sig) => (ref, Signal (eval(sig(), namedExpressions - ref)))
+    }
+  }
+
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => eval(getReferenceExpr(name, references), references)
+      case Plus(a, b) => eval(a, references) + eval(b,references)
+      case Minus(a, b) => eval(a, references) - eval(b,references)
+      case Times(a, b) => eval(a, references) * eval(b,references)
+      case Divide(a, b) =>
+        val d = eval(b, references)
+        if (d != 0) eval(a, references) / d else Double.NaN
+      case _ => Double.NaN
+    }
+  }
+
+  /** Get the Expr for a referenced variables.
+   *  If the variable is not known, returns a literal NaN.
+   */
+  private def getReferenceExpr(name: String,
+      references: Map[String, Signal[Expr]]) = {
+    references.get(name).fold[Expr] {
+      Literal(Double.NaN)
+    } { exprSignal =>
+      exprSignal()
+    }
+  }
+}
---
+++ calculator/src/test/scala/calculator/CalculatorSuite.scala
@@ -0,0 +1,119 @@
+package calculator
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSuite, _}
+
+@RunWith(classOf[JUnitRunner])
+class CalculatorSuite extends FunSuite with ShouldMatchers {
+
+  /** ****************
+    * * TWEET LENGTH **
+    * *****************/
+
+//    def tweetLength(text: String): Int =
+//      text.codePointCount(0, text.length)
+//
+//    test("tweetRemainingCharsCount with a constant signal") {
+//      val result = TweetLength.tweetRemainingCharsCount(Var("hello world"))
+//      assert(result() == MaxTweetLength - tweetLength("hello world"))
+//
+//      val tooLong = "foo" * 200
+//      val result2 = TweetLength.tweetRemainingCharsCount(Var(tooLong))
+//      assert(result2() == MaxTweetLength - tweetLength(tooLong))
+//    }
+//
+//    test("tweetRemainingCharsCount with a supplementary char") {
+//      val result = TweetLength.tweetRemainingCharsCount(Var("foo blabla \uD83D\uDCA9 bar"))
+//      assert(result() == MaxTweetLength - tweetLength("foo blabla \uD83D\uDCA9 bar"))
+//    }
+//
+//
+//    test("colorForRemainingCharsCount with a constant signal") {
+//      val resultGreen1 = TweetLength.colorForRemainingCharsCount(Var(52))
+//      assert(resultGreen1() == "green")
+//      val resultGreen2 = TweetLength.colorForRemainingCharsCount(Var(15))
+//      assert(resultGreen2() == "green")
+//
+//      val resultOrange1 = TweetLength.colorForRemainingCharsCount(Var(12))
+//      assert(resultOrange1() == "orange")
+//      val resultOrange2 = TweetLength.colorForRemainingCharsCount(Var(0))
+//      assert(resultOrange2() == "orange")
+//
+//      val resultRed1 = TweetLength.colorForRemainingCharsCount(Var(-1))
+//      assert(resultRed1() == "red")
+//      val resultRed2 = TweetLength.colorForRemainingCharsCount(Var(-5))
+//      assert(resultRed2() == "red")
+//    }
+
+
+  test("cyclic") {
+    val m1 = Map(
+      "a" -> Signal[Expr](Plus(Ref("a"),Literal(1.0))),
+      "d" -> Signal[Expr](Minus(Literal(5.0),Literal(3.0)))
+    )
+
+    val m2 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "b" -> Signal[Expr](Divide(Ref("c"),Ref("d"))),
+      "c" -> Signal[Expr](Times(Literal(5.0),Ref("a"))),
+      "d" -> Signal[Expr](Minus(Literal(5.0),Literal(3.0)))
+    )
+
+    val m3 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "d" -> Signal[Expr](Minus(Literal(5.0),Literal(3.0)))
+    )
+
+    val m4 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "b" -> Signal[Expr](Times(Ref("c"),Ref("d"))),
+      "c" -> Signal[Expr](Plus(Literal(5.0),Ref("d"))),
+      "d" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0)))
+    )
+
+    val m5 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "b" -> Signal[Expr](Times(Ref("c"),Ref("d"))),
+      "c" -> Signal[Expr](Plus(Literal(4.0),Literal(3.0))),
+      "d" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0)))
+    )
+
+    val m6 = Map(
+      "a" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0))),
+      "b" -> Signal[Expr](Plus(Ref("a"),Literal(1.0)))
+    )
+
+    val m7 = Map(
+      "a" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0))),
+      "b" -> Signal[Expr](Literal(1.0))
+    )
+
+    val m8 = Map(
+      "a" -> Signal[Expr](Literal(5.0)),
+      "b" -> Signal[Expr](Literal(1.0))
+    )
+
+    val m9 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "b" -> Signal[Expr](Times(Literal(5.0),Ref("d"))),
+      "c" -> Signal[Expr](Plus(Literal(5.0),Ref("d"))),
+      "d" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0)))
+    )
+
+    val m10 = Map(
+      "a" -> Signal[Expr](Plus(Ref("b"),Literal(1.0))),
+      "b" -> Signal[Expr](Times(Literal(5.0),Ref("d"))),
+      "c" -> Signal[Expr](Plus(Literal(4.0),Literal(3.0))),
+      "d" -> Signal[Expr](Minus(Literal(4.0),Literal(3.0)))
+    )
+
+
+    val cal = Calculator.computeValues(m4)
+    cal.map(p => (p._1, p._2())) foreach println
+  }
+
+
+
+
+}
