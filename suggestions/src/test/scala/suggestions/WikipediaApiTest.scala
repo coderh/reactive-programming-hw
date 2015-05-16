d80a175475ec8accbece7ac5ffa854905acb7800
invkrh <invkrh@gmail.com>
2015-05-17 01:40:28 +0200

    [suggestions] Finished

---
+++ suggestions/src/test/scala/suggestions/WikipediaApiTest.scala
@@ -0,0 +1,72 @@
+package suggestions
+
+
+import language.postfixOps
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Try, Success, Failure}
+import rx.lang.scala._
+import org.scalatest._
+import gui._
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class WikipediaApiTest extends FunSuite {
+
+  object mockApi extends WikipediaApi {
+    def wikipediaSuggestion(term: String) = Future {
+      if (term.head.isLetter) {
+        for (suffix <- List(" (Computer Scientist)", " (Footballer)")) yield term + suffix
+      } else {
+        List(term)
+      }
+    }
+
+    def wikipediaPage(term: String) = Future {
+      "Title: " + term
+    }
+  }
+
+  import mockApi._
+
+  test("WikipediaApi should make the stream valid using sanitized") {
+    val notvalid = Observable.just("erik", "erik meijer", "martin")
+    val valid = notvalid.sanitized
+
+    var count = 0
+    var completed = false
+
+    val sub = valid.subscribe(
+      term => {
+        assert(term.forall(_ != ' '))
+        count += 1
+      },
+      t => assert(false, s"stream error $t"),
+      () => completed = true
+    )
+    assert(completed && count == 3, "completed: " + completed + ", event count: " + count)
+  }
+
+  test("WikipediaApi should correctly use concatRecovered") {
+    val requests = Observable.just(1, 2, 3, 4, 5)
+    val remoteComputation = (num: Int) => if (num != 4) Observable.just(num) else Observable.error(new Exception)
+    // val remoteComputation = (num: Int) => Observable.just(num, num, num)
+    val responses = requests concatRecovered remoteComputation
+    //    val sum = responses.foldLeft(0) { (acc, tn) =>
+    //      tn match {
+    //        case Success(n) => acc + n
+    //        case Failure(t) => throw t
+    //      }
+    //    }
+    //    var total = -1
+    responses.subscribe {
+      x => println(x)
+    }
+    //assert(total == (1 + 1 + 2 + 1 + 2 + 3), s"Sum: $total")
+  }
+
+}
